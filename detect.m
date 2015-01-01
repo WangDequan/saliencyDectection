b5e028654fa44e2fb2e49d45f6e373fc759cd198
ZhangYiqing <zhang-yq10@mails.tsinghua.edu.cn>
2015-01-01 15:54:31 +0800

    finish the basic version which is slow

--- detect.m
+++ detect.m
@@ -1,19 +1,28 @@
-im = imread('42.jpg');
+im = imread('42_meitu_2.jpg');
 imshow(im)
-diffIm = im;
-lab = RGB2Lab(im);
-imshow(lab)
 
-% for i = 1:size(im,1)
-%    for j = 1:size(im,2)
-%       theDiff = 0;
-%       for ii = 1:size(im,1)
-%            for jj = 1:size(im,2)
-%               if(i ~= ii || j ~= jj)
-%                   
-%               end
-%            end
-%       end
-%       
-%    end
-% end
+lab = RGB2Lab(im);
+saliency = zeros(size(im,1),size(im,2));
+
+for i = 1:size(im,1)
+   for j = 1:size(im,2)
+      diffSum = 0;
+      for ii = 1:size(im,1)
+           for jj = 1:size(im,2)
+              theDiff = lab(i,j,:)-lab(ii,jj,:);
+              diff = [theDiff(1),theDiff(2),theDiff(3)];
+              diffNorm = norm(diff);
+              diffSum = diffSum + diffNorm;
+              
+           end
+      end
+      saliency(i,j) = diffSum;
+      
+   end
+end
+%normalize it
+theMax = max(saliency(:));
+saliency = saliency / theMax;
+
+figure
+imshow(saliency)
